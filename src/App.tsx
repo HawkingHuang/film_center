3b607d16f7f18784331031348d8b17bf2755d068
HawkingHuang <math0915@gmail.com>
2026-01-19 14:03:44 +0800

    chore: add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,51 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Search from "./pages/Search/Search";
+import Login from "./pages/Login/Login";
+import Signup from "./pages/Signup/Signup";
+import Movie from "./pages/Movie/Movie";
+import Actor from "./pages/Actor/Actor";
+import User from "./pages/User/User";
+import AppLayout from "./ui/AppLayout";
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/search",
+        element: <Search />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/signup",
+        element: <Signup />,
+      },
+      {
+        path: "/movies/:id",
+        element: <Movie />,
+      },
+      {
+        path: "/actors/:id",
+        element: <Actor />,
+      },
+      {
+        path: "/user",
+        element: <User />,
+      },
+    ],
+  },
+]);
+
 function App() {
-  return null;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ src/pages/Actor/Actor.tsx
@@ -0,0 +1,5 @@
+function Actor() {
+  return <div></div>;
+}
+
+export default Actor;
